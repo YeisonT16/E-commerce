feabd355913bb610e4bd36f11b4773e47cdf55d7
Yeison Silva <yeisonsilvat06@gmail.com>
2024-05-28 21:25:24 -0500

    Correciones en los estilos del componente OrderCard

--- src/Components/OrderCard.jsx
+++ src/Components/OrderCard.jsx
@@ -10,7 +10,7 @@ function OrderCard(props){
     } = props
 
     return(
-        <div className="flex justify-between items-center mb-3 rounded-lg">
+        <div className="flex items-center mb-3 rounded-lg">
             <div className="flex items-center gap-2">
                 <figure className="size-14">
                     <img className="size-full rounded-lg object-cover"
@@ -19,8 +19,8 @@ function OrderCard(props){
                     />
                 </figure>
             </div>
-            <div className="flex justify-items-start px-2 py-1">
-                <p className="text-sm font-light max-h-14 overflow-hidden text-yellow-500">{name}</p>
+            <div className="flex justify-items-start px-2 py-1 max-w-[90] min-w-[]">
+                <p className="text-sm font-medium max-h-14 overflow-hidden text-yellow-600/90">{name}</p>
             </div>
             <div className="flex items-center gap-2">
                 <p className="text-lg font-medium text-red-500/80">{price}</p>
