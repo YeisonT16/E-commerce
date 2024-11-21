895dfef259bab21f5837528a3fb8c4016eeed44d
Yeison Silva <yeisonsilvat06@gmail.com>
2024-11-20 22:58:51 -0500

    Ajustes en los colores de los botones left y right

--- src/Components/OrdersCard.jsx
+++ src/Components/OrdersCard.jsx
@@ -18,12 +18,14 @@ function OrdersCard(props){
                     <span className='font-medium text-green-300/70'>{currentDate()}</span>
                     <span className="font-light text-sky-300/80">{totalProducts} articles</span>
                 </p>
-                <p className="flex items-center gap-2">
-                    <span className="font-medium text-2xl text-red-500/70">{totalPrice}</span>
-                    <button className="flex items-center text-white bg-transparent hover:border-l-gray-900 size-6 hover:border rounded-md  hover:text-blue-400 transition-transform hover:scale-125">
+                <div className="flex items-center gap-2 p-2  size-fit">
+                    <div className="mr-4">
+                        <span className="font-medium text-2xl text-red-500/70">{totalPrice}</span>
+                    </div>
+                    <button className="flex items-center text-white bg-transparent hover:bg-zinc-900 hover:border-zinc-900 size-6 hover:border rounded-md hover:text-blue-400 transition-transform hover:scale-125">
                         <ArrowRightIcon className="size-7" />
                     </button>
-                </p>
+                </div>
             </p>
         </div>
     )
